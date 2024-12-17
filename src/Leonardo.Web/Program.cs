0220130a59cfef9b4a8db970b87297ec5fdee38a
Lucien <lucien.letailleur@gmail.com>
2024-12-17 16:48:31 +0100

    Commit With Swagger

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -1,16 +1,28 @@
-using Leonardo;
 using System.Text.Json.Serialization;
+using Leonardo;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FibonacciDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<Fibonacci>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World 2!");
 
-app.MapGet("/fibonacci", async (Fibonacci fibonacci) =>
+app.MapGet("/fibonacci", async (Fibonacci fibonacci, ILogger<Program> logger) =>
 {
+    logger.LogInformation("Application Name : {ApplicationConfigName}", "Leonardo");
     var result = await fibonacci.RunAsync(["42"]);
     return Results.Json(result, FibonacciContext.Default.ListFibonacciResult);
 });
